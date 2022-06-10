552ec4dac0709f5948f410d56396dc5347d8157a
Gabriel Aguilar <gabo6252@gmail.com>
2022-06-10 11:18:26 -0400

    update cors

--- app.js
+++ app.js
@@ -22,7 +22,15 @@ var cloudinary = require("cloudinary").v2;
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
